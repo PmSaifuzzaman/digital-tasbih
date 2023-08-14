d42e4203f5b21b16bb26303f6ef17af36c27e6a3
Saifuzzaman <pmsaifuzzaman@gmail.com>
2023-08-14 14:58:42 +0600

    all variable are initialized

--- scripts/first.js
+++ scripts/first.js
@@ -1,1 +1,13 @@
-console.log('First js integration');
+const subhanAllahDisplay = document.getElementById('subhanAllahDisplay');
+const subhanAllahIncrimentBtn = document.getElementById('subhanAllahIncrimentBtn');
+const subhanAllahDecrimentBtn = document.getElementById('subhanAllahDecrimentBtn');
+
+const alhamdulillahDisplay = document.getElementById('alhamdulillahDisplay');
+const alhamdulillahIncrimentBtn = document.getElementById('alhamdulillahIncrimentBtn');
+const alhamdulillahDecrimentBtn = document.getElementById('alhamdulillahDecrimentBtn');
+
+const allahAkberDisplay = document.getElementById('allahAkberDisplay');
+const allahAkberIncrimentBtn = document.getElementById('allahAkberIncrimentBtn');
+const allahAkberDecrimentBtn = document.getElementById('allahAkberDecrimentBtn');
+
+const resetBtn = document.getElementById('resetBtn');
